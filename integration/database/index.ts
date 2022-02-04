5c2fc55644ea6de138382743410a0bdb922b9d8b
csikb <22349790+csikb@users.noreply.github.com>
2022-02-04 09:07:42 +0100

    add integration test

--- integration/database/index.ts
+++ integration/database/index.ts
@@ -1,15 +1,16 @@
 import { Pool } from 'pg'
-import { EventEntity, eventQueries } from './event.queries'
+import { EventEntity, insertEventQuery } from './event.queries'
 import { insertMemberQuery, MemberEntity } from './member.queries'
 import { CrewEntity, insertCrewQuery } from './video-crew.queries'
 import { insertVideoQuery, VideoEntity } from './video.queries'
+import { EventVideoEntity, insertEventVideoQuery } from './event-video.queries'
 
 export class DbUtils {
   pool = new Pool({ connectionString: globalThis.baseUrl.db })
 
   async addEvents(events: EventEntity[]) {
     const client = await this.pool.connect()
-    await client.query(eventQueries(events))
+    await client.query(insertEventQuery(events))
     await client.release()
   }
   async addMembers(members: MemberEntity[]) {
@@ -25,6 +26,11 @@ export class DbUtils {
   async addVideoCrew(crews: CrewEntity[]) {
     const client = await this.pool.connect()
     await client.query(insertCrewQuery(crews))
+    await client.release()
+  }
+  async addEventVideos(eventVideos: EventVideoEntity[]) {
+    const client = await this.pool.connect()
+    await client.query(insertEventVideoQuery(eventVideos))
     await client.release()
   }
   async beforeEach() {
