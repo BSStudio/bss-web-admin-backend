638e53e2a0e23c1f4da824e6dd6219a986e9feaf
csikb <22349790+csikb@users.noreply.github.com>
2022-02-04 01:10:57 +0100

    implement add and remove video to/from event

---
+++ service/src/main/kotlin/hu/bsstudio/bssweb/eventvideo/service/DefaultEventVideoService.kt
@@ -0,0 +1,26 @@
+package hu.bsstudio.bssweb.eventvideo.service
+
+import hu.bsstudio.bssweb.event.model.DetailedEvent
+import hu.bsstudio.bssweb.event.service.EventService
+import hu.bsstudio.bssweb.eventvideo.entity.EventVideoEntity
+import hu.bsstudio.bssweb.eventvideo.repository.EventVideoRepository
+import java.util.Optional
+import java.util.UUID
+
+class DefaultEventVideoService(
+    private val repository: EventVideoRepository,
+    private val eventService: EventService
+) : EventVideoService {
+
+    override fun addVideoToEvent(eventId: UUID, videoId: UUID): Optional<DetailedEvent> {
+        return EventVideoEntity(eventId = eventId, videoId = videoId)
+            .let(repository::save)
+            .let(EventVideoEntity::eventId)
+            .let(eventService::findEventById)
+    }
+
+    override fun removeVideoFromEvent(eventId: UUID, videoId: UUID) {
+        return EventVideoEntity(eventId = eventId, videoId = videoId)
+            .let(repository::deleteById)
+    }
+}
---
+++ service/src/test/kotlin/hu/bsstudio/bssweb/eventvideo/service/DefaultEventVideoServiceTest.kt
@@ -0,0 +1,55 @@
+package hu.bsstudio.bssweb.eventvideo.service
+
+import hu.bsstudio.bssweb.event.model.DetailedEvent
+import hu.bsstudio.bssweb.event.service.EventService
+import hu.bsstudio.bssweb.eventvideo.entity.EventVideoEntity
+import hu.bsstudio.bssweb.eventvideo.repository.EventVideoRepository
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.util.Optional
+import java.util.UUID
+
+internal class DefaultEventVideoServiceTest {
+
+    @MockK
+    private lateinit var mockRepository: EventVideoRepository
+    @MockK
+    private lateinit var mockEventService: EventService
+
+    private lateinit var underTest: DefaultEventVideoService
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+        underTest = DefaultEventVideoService(mockRepository, mockEventService)
+    }
+
+    @Test
+    fun addVideoToEvent() {
+        every { mockRepository.save(EVENT_VIDEO_ENTITY) } returns EVENT_VIDEO_ENTITY
+        every { mockEventService.findEventById(EVENT_ID) } returns Optional.of(DETAILED_EVENT)
+
+        val response = underTest.addVideoToEvent(EVENT_ID, VIDEO_ID)
+
+        assertThat(response).hasValue(DETAILED_EVENT)
+    }
+
+    @Test
+    fun removeVideoFromEvent() {
+        every { mockRepository.deleteById(EVENT_VIDEO_ENTITY) } returns Unit
+
+        underTest.removeVideoFromEvent(EVENT_ID, VIDEO_ID)
+    }
+
+    private companion object {
+        private val EVENT_ID = UUID.fromString("01234567-0123-0123-0123-0123456789ab")
+        private val VIDEO_ID = UUID.fromString("11234567-0123-0123-0123-0123456789ab")
+        private val EVENT_VIDEO_ENTITY = EventVideoEntity(eventId = EVENT_ID, videoId = VIDEO_ID)
+        private val DETAILED_EVENT = DetailedEvent(id = EVENT_ID, url = "url", title = "title", description = "description", date = LocalDate.of(2022, 1, 1), visible = true, videos = listOf())
+    }
+}
