d90d173be0c2580e12d5de7f707b0a1a6d03c371
Bence Csik <22349790+csikb@users.noreply.github.com>
2022-07-05 18:46:17 +0100

    fix typo

--- app/src/main/kotlin/hu/bsstudio/bssweb/ServletInitializer.kt
+++ app/src/main/kotlin/hu/bsstudio/bssweb/ServletInitializer.kt
@@ -3,7 +3,7 @@ package hu.bsstudio.bssweb
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
-class ServletInitializer: SpringBootServletInitializer() {
+class ServletInitializer : SpringBootServletInitializer() {
 
     override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
         return builder.sources(BssWebApplication::class.java)
