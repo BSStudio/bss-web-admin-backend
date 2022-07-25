62f57b9d32c3515cfdf9e4bf7d4eb4923cb82eb9
Bence Csik <22349790+csikb@users.noreply.github.com>
2022-07-25 20:54:13 +0100

    update configs

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 dependencies {
     api(project(":service"))
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation(libs.springdocOpenapiUi)
