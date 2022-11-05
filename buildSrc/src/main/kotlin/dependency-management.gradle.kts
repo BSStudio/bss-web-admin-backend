21083f8ecfd59c89c08ecbb343a25645bf5762a8
Bence Csik <22349790+csikb@users.noreply.github.com>
2022-11-05 19:47:44 +0000

    Update dependency-management.gradle.kts

--- buildSrc/src/main/kotlin/dependency-management.gradle.kts
+++ buildSrc/src/main/kotlin/dependency-management.gradle.kts
@@ -7,13 +7,10 @@ repositories {
     mavenCentral()
 }
 
-extra["springCloudVersion"] = "2021.0.5"
-extra["mockkVersion"] = "1.13.2"
-
 dependencyManagement {
     imports {
         // https://docs.spring.io/spring-boot/docs/current/gradle-plugin/reference/htmlsingle/#managing-dependencies-dependency-management-plugin-using-in-isolation
         mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2021.0.5")
     }
 }
