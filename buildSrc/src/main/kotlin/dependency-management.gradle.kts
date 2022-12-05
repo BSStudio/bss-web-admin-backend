650b51cdf458363622ca5544f30b7e9ac2bd17ba
Bence Csik <22349790+csikb@users.noreply.github.com>
2022-12-05 11:20:08 +0000

    wip

--- buildSrc/src/main/kotlin/dependency-management.gradle.kts
+++ buildSrc/src/main/kotlin/dependency-management.gradle.kts
@@ -5,12 +5,13 @@ plugins {
 
 repositories {
     mavenCentral()
+    maven { setUrl("https://repo.spring.io/milestone") } // todo remove later
 }
 
 dependencyManagement {
     imports {
         // https://docs.spring.io/spring-boot/docs/current/gradle-plugin/reference/htmlsingle/#managing-dependencies-dependency-management-plugin-using-in-isolation
         mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2021.0.5")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2022.0.0-RC2")
     }
 }
--- server/client/src/main/java/hu/bsstudio/bssweb/fileserver/client/FileApiClient.kt
+++ server/client/src/main/java/hu/bsstudio/bssweb/fileserver/client/FileApiClient.kt
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
-@FeignClient("member")
+@FeignClient(name = "member", url = "")
 interface FileApiClient {
 
     @RequestMapping(method = [RequestMethod.POST], path = ["/api/v1/member"])
