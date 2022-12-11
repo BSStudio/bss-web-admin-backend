526ae8442cb7c03d11aadaee92903811f8a0a52f
Bence Csik <22349790+csikb@users.noreply.github.com>
2022-12-11 19:23:22 +0100

    shorter test

--- server/web/src/test/kotlin/hu/bsstudio/bssweb/videocrew/controller/VideoCrewControllerTest.kt
+++ server/web/src/test/kotlin/hu/bsstudio/bssweb/videocrew/controller/VideoCrewControllerTest.kt
@@ -1,19 +1,17 @@
 package hu.bsstudio.bssweb.videocrew.controller
 
-import hu.bsstudio.bssweb.member.model.SimpleMember
 import hu.bsstudio.bssweb.video.model.DetailedVideo
-import hu.bsstudio.bssweb.videocrew.model.VideoCrew
 import hu.bsstudio.bssweb.videocrew.model.VideoCrewRequest
 import hu.bsstudio.bssweb.videocrew.service.VideoCrewService
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.http.HttpStatus
-import java.time.LocalDate
 import java.util.Optional
 import java.util.UUID
 
@@ -76,20 +74,10 @@ internal class VideoCrewControllerTest {
     }
 
     private companion object {
-        private val VIDEO_ID = UUID.fromString("01234567-0123-0123-0123-0123456789ab")
-        private const val POSITION = "position1"
-        private val MEMBER_ID = UUID.fromString("11234567-0123-0123-0123-0123456789ab")
-        private val MEMBER = SimpleMember(MEMBER_ID, "name")
+        private val VIDEO_ID = mockk<UUID>()
+        private const val POSITION = "position"
+        private val MEMBER_ID = mockk<UUID>()
         private val VIDEO_CREW_REQUEST = VideoCrewRequest(VIDEO_ID, POSITION, MEMBER_ID)
-        private val SIMPLE_VIDEO_CREW = VideoCrew(VIDEO_ID, POSITION, MEMBER)
-        private val DETAILED_VIDEO = DetailedVideo(
-            id = VIDEO_ID,
-            url = "url",
-            title = "title",
-            description = "description",
-            visible = true,
-            uploadedAt = LocalDate.of(2022, 1, 1),
-            crew = listOf(SIMPLE_VIDEO_CREW)
-        )
+        private val DETAILED_VIDEO = mockk<DetailedVideo>()
     }
 }
