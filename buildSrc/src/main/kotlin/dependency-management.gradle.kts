0990c9ca365f5703ecd2590b59a2186673287e75
Bence Csik <22349790+csikb@users.noreply.github.com>
2022-12-14 11:02:38 +0100

    remove alpha version of spring cloud

--- buildSrc/src/main/kotlin/dependency-management.gradle.kts
+++ buildSrc/src/main/kotlin/dependency-management.gradle.kts
@@ -5,13 +5,12 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven { setUrl("https://repo.spring.io/milestone") } // todo remove later
 }
 
 dependencyManagement {
     imports {
         // https://docs.spring.io/spring-boot/docs/current/gradle-plugin/reference/htmlsingle/#managing-dependencies-dependency-management-plugin-using-in-isolation
         mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2022.0.0-RC2")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2021.0.5")
     }
 }
