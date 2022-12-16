ee99c1f7af07c9339e69243bbd4349e8887f50ec
Bence Csik <22349790+csikb@users.noreply.github.com>
2022-12-16 11:50:46 +0100

    fix spring cloud version (#152)

--- buildSrc/src/main/kotlin/dependency-management.gradle.kts
+++ buildSrc/src/main/kotlin/dependency-management.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 plugins {
     id("kotlin-conventions")
     id("io.spring.dependency-management")
@@ -5,12 +7,13 @@ plugins {
 
 repositories {
     mavenCentral()
+    maven { url = URI.create("https://repo.spring.io/milestone") }
 }
 
 dependencyManagement {
     imports {
         // https://docs.spring.io/spring-boot/docs/current/gradle-plugin/reference/htmlsingle/#managing-dependencies-dependency-management-plugin-using-in-isolation
         mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2021.0.5")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2022.0.0-RC3")
     }
 }
