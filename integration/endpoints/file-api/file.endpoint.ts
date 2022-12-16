4a12e5c7ed59a8566416b0466c011d008d464fd9
Bence Csik <22349790+csikb@users.noreply.github.com>
2022-12-16 09:52:06 +0100

    Add default Wiremock mapping (#151)

--- integration/endpoints/file-api/file.endpoint.ts
+++ integration/endpoints/file-api/file.endpoint.ts
@@ -7,9 +7,9 @@ export interface Count {
 export class FileEndpoint {
   private static client = client
 
-  private static mockBody = (url: string) => ({
+  private static mockBody = (method: string, url: string) => ({
     request: {
-      method: 'POST',
+      method,
       url,
       bodyPatterns: [
         {
@@ -32,8 +32,8 @@ export class FileEndpoint {
     },
   })
 
-  private static verifyBody = (url: string) => ({
-    method: 'PUT',
+  private static verifyBody = (method: string, url: string) => ({
+    method,
     url,
     bodyPatterns: [
       {
@@ -50,45 +50,45 @@ export class FileEndpoint {
   }
 
   static mockCreateVideoFolder() {
-    return this.client.post('/__admin/mappings', this.mockBody('/api/v1/video'))
+    return this.client.post('/__admin/mappings', this.mockBody('POST', '/api/v1/video'))
   }
 
   static verifyCreateVideoFolder() {
     return this.client
-      .post<Count>('/__admin/requests/count', this.verifyBody('/api/v1/video'))
+      .post<Count>('/__admin/requests/count', this.verifyBody('POST', '/api/v1/video'))
       .then(({ data }) => data)
       .then(({ count }) => count)
   }
 
   static mockUpdateVideoFolder() {
-    return this.client.post('/__admin/mappings', this.mockBody('/api/v1/video'))
+    return this.client.post('/__admin/mappings', this.mockBody('PUT', '/api/v1/video'))
   }
 
   static verifyUpdateVideoFolder() {
     return this.client
-      .post<Count>('/__admin/requests/count', this.verifyBody('/api/v1/video'))
+      .post<Count>('/__admin/requests/count', this.verifyBody('PUT', '/api/v1/video'))
       .then(({ data }) => data)
       .then(({ count }) => count)
   }
 
   static mockCreateMemberFolder() {
-    return this.client.post('/__admin/mappings', this.mockBody('/api/v1/member'))
+    return this.client.post('/__admin/mappings', this.mockBody('POST', '/api/v1/member'))
   }
 
   static verifyCreateMemberFolder() {
     return this.client
-      .post<Count>('/__admin/requests/count', this.verifyBody('/api/v1/member'))
+      .post<Count>('/__admin/requests/count', this.verifyBody('POST', '/api/v1/member'))
       .then(({ data }) => data)
       .then(({ count }) => count)
   }
 
   static mockUpdateMemberFolder() {
-    return this.client.post('/__admin/mappings', this.mockBody('/api/v1/member'))
+    return this.client.post('/__admin/mappings', this.mockBody('PUT', '/api/v1/member'))
   }
 
   static verifyUpdateMemberFolder() {
     return this.client
-      .post<Count>('/__admin/requests/count', this.verifyBody('/api/v1/member'))
+      .post<Count>('/__admin/requests/count', this.verifyBody('PUT', '/api/v1/member'))
       .then(({ data }) => data)
       .then(({ count }) => count)
   }
