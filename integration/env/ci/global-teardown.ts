811d37a6eebde1c69b95c0ae0037399d52c0fc68
Bence Csik <22349790+csikb@users.noreply.github.com>
2023-07-13 18:41:47 +0100

    remove integration test

--- integration/env/ci/global-teardown.ts
+++
@@ -1,9 +0,0 @@
-import { rimraf } from 'rimraf'
-import jestTempDir from './jest-temp-dir'
-
-export default async function () {
-  // remove temp folder
-  await rimraf(jestTempDir)
-  // stop containers
-  await globalThis.dockerComposeEnvironment.down()
-}
--- integration/env/ci/jest-temp-dir.ts
+++
@@ -1,4 +0,0 @@
-import * as os from 'os'
-import * as path from 'path'
-
-export default path.join(os.tmpdir(), 'jest_testcontainers_global_setup')
