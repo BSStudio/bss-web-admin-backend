e65c9ba132850697aead30904ecc97508d3e3a05
Bence Csik <22349790+csikb@users.noreply.github.com>
2023-07-13 15:45:30 +0100

    Update server/web/src/main/kotlin/hu/bsstudio/bssweb/security/config/SecurityConfig.kt

--- server/web/src/main/kotlin/hu/bsstudio/bssweb/security/config/SecurityConfig.kt
+++ server/web/src/main/kotlin/hu/bsstudio/bssweb/security/config/SecurityConfig.kt
@@ -6,7 +6,6 @@ import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebSecurity
