0f33fb568d4a9d2ffcdf429a5ea9c2e0a0709ba3
Bence Csik <22349790+csikb@users.noreply.github.com>
2023-07-21 13:48:43 +0100

    move sql

--- server/data/src/main/resources/db/migration/postgresql/V0__init.sql
+++ server/data/src/main/resources/db/migration/postgresql/V0__init.sql
@@ -1,18 +1,16 @@
-CREATE SCHEMA bss_web;
-
-CREATE TABLE bss_web.status
+CREATE TABLE status
 (
     name VARCHAR(250) PRIMARY KEY
 );
 
-INSERT INTO bss_web.status
+INSERT INTO status
 VALUES ('ALUMNI'),
        ('ACTIVE_ALUMNI'),
        ('MEMBER'),
        ('MEMBER_CANDIDATE'),
        ('MEMBER_CANDIDATE_CANDIDATE');
 
-CREATE TABLE bss_web.member
+CREATE TABLE member
 (
     id          UUID PRIMARY KEY             DEFAULT gen_random_uuid(),
     url         VARCHAR(250) UNIQUE NOT NULL,
@@ -21,13 +19,13 @@ CREATE TABLE bss_web.member
     description VARCHAR(2000)       NOT NULL DEFAULT '',
     joined_at   DATE                NOT NULL DEFAULT current_date,
     role        VARCHAR(250)        NOT NULL DEFAULT '',
-    status      VARCHAR(250)        NOT NULL DEFAULT 'MEMBER_CANDIDATE_CANDIDATE' REFERENCES bss_web.status (name),
+    status      VARCHAR(250)        NOT NULL DEFAULT 'MEMBER_CANDIDATE_CANDIDATE' REFERENCES status (name),
     archived    BOOLEAN             NOT NULL DEFAULT FALSE,
     CONSTRAINT url_not_empty CHECK (trim(url) != ''),
     CONSTRAINT name_not_empty CHECK (trim(name) != '')
 );
 
-CREATE TABLE bss_web.video
+CREATE TABLE video
 (
     id            UUID PRIMARY KEY             DEFAULT gen_random_uuid(),
     url           VARCHAR(250) UNIQUE NOT NULL,
@@ -39,16 +37,16 @@ CREATE TABLE bss_web.video
     CONSTRAINT title_not_empty CHECK (trim(title) != '')
 );
 
-CREATE TABLE bss_web.crew
+CREATE TABLE crew
 (
-    video_id  UUID REFERENCES bss_web.video (id),
-    member_id UUID REFERENCES bss_web.member (id),
+    video_id  UUID REFERENCES video (id),
+    member_id UUID REFERENCES member (id),
     position  VARCHAR(250) NOT NULL,
     PRIMARY KEY (video_id, member_id, position),
     CONSTRAINT position_not_empty CHECK (trim(position) != '')
 );
 
-CREATE TABLE bss_web.event
+CREATE TABLE event
 (
     id          UUID PRIMARY KEY             DEFAULT gen_random_uuid(),
     url         VARCHAR(250) UNIQUE NOT NULL,
@@ -60,9 +58,9 @@ CREATE TABLE bss_web.event
     CONSTRAINT title_not_empty CHECK (trim(title) != '')
 );
 
-CREATE TABLE bss_web.event_video
+CREATE TABLE event_video
 (
-    event_id UUID REFERENCES bss_web.event (id),
-    video_id UUID REFERENCES bss_web.video (id),
+    event_id UUID REFERENCES event (id),
+    video_id UUID REFERENCES video (id),
     PRIMARY KEY (event_id, video_id)
 );
