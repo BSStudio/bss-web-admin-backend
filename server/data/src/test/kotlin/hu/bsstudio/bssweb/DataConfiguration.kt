39d2809ddf5348eb53069ece334340606ebcae8a
Bence Csik <22349790+csikb@users.noreply.github.com>
2023-07-27 16:03:36 +0100

    refactor

--- server/data/src/test/kotlin/hu/bsstudio/bssweb/DataConfiguration.kt
+++ server/data/src/test/kotlin/hu/bsstudio/bssweb/DataConfiguration.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.test.context.TestPropertySource
 
 @Configuration
 @EnableAutoConfiguration
