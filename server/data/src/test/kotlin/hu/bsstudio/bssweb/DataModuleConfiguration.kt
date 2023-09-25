5bc6125946ba9817842f7d7f2def4ceb4044b84e
Bence Csik <22349790+csikb@users.noreply.github.com>
2023-09-25 17:03:50 +0100

    add component scan

--- server/data/src/test/kotlin/hu/bsstudio/bssweb/DataModuleConfiguration.kt
+++ server/data/src/test/kotlin/hu/bsstudio/bssweb/DataModuleConfiguration.kt
@@ -2,9 +2,11 @@ package hu.bsstudio.bssweb
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa
+import org.springframework.context.annotation.ComponentScan
 
 @EnableAutoConfiguration
 @AutoConfigureDataJpa
+@ComponentScan("hu.bsstudio.bssweb.**.repository")
 class DataModuleConfiguration
 // this class is used to autoconfigure the data layer for testing
 // it's defined in the test scope as the main class will be configured by the main Spring Boot class
