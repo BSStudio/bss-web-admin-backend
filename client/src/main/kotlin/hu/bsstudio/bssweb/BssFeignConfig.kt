d806b858787448d9049a1ec87ba6ff9154803f30
Bence Csik <22349790+csikb@users.noreply.github.com>
2023-09-27 09:18:28 +0100

    refactor

--- client/src/main/kotlin/hu/bsstudio/bssweb/BssFeignConfig.kt
+++ client/src/main/kotlin/hu/bsstudio/bssweb/BssFeignConfig.kt
@@ -1,26 +1,11 @@
 package hu.bsstudio.bssweb
 
-import feign.auth.BasicAuthRequestInterceptor
-import org.bouncycastle.cms.RecipientId.password
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.cloud.openfeign.FeignAutoConfiguration
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 
-@EnableFeignClients("hu.bsstudio.bssweb.**.client", defaultConfiguration = [BssFeignConfig.DefaultFeignConfig::class])
+@EnableFeignClients("hu.bsstudio.bssweb.**.client", defaultConfiguration = [DefaultFeignConfig::class])
+// todo remove below configuration, once Spring Cloud Feign is updated
 @ImportAutoConfiguration(FeignAutoConfiguration::class, HttpMessageConvertersAutoConfiguration::class)
-class BssFeignConfig {
-
-    @Configuration
-    class DefaultFeignConfig(
-        @Value("\${bss.client.username}") val username: String,
-        @Value("\${bss.client.password}") val password: String
-    ) {
-
-        @Bean
-        fun interceptor() = BasicAuthRequestInterceptor(this.username, this.password)
-    }
-}
+class BssFeignConfig
---
+++ client/src/main/kotlin/hu/bsstudio/bssweb/DefaultFeignConfig.kt
@@ -0,0 +1,16 @@
+package hu.bsstudio.bssweb
+
+import feign.auth.BasicAuthRequestInterceptor
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class DefaultFeignConfig(
+    @Value("\${bss.client.username}") val username: String,
+    @Value("\${bss.client.password}") val password: String
+) {
+
+    @Bean
+    fun interceptor() = BasicAuthRequestInterceptor(this.username, this.password)
+}
