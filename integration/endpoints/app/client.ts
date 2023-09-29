cf54abc91e8580748e8bb81c7608c3cd36722d0f
Bence Csik <22349790+csikb@users.noreply.github.com>
2023-09-29 18:39:07 +0100

    Remove typescript integration test (#209)
    * remove integration test
    
    * Update .circleci/config.yml
    
    * Apply suggestions from code review
    
    * refactor

--- integration/endpoints/app/client.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export const client = axios.create({
-  baseURL: globalThis.baseUrl.app,
-  auth: { username: 'user', password: 'password' },
-  validateStatus: null,
-})
--- integration/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { StartedDockerComposeEnvironment } from 'testcontainers'
-
-declare global {
-  /* eslint-disable no-var */
-  var dockerComposeEnvironment: StartedDockerComposeEnvironment
-  var baseUrl: { app: string; db: string; fileApi: string }
-  /* eslint-enable no-var */
-}
