da33f72cc8ac9596b03be2459eee587a922e9e8b
Bence Csik <22349790+csikb@users.noreply.github.com>
2023-09-29 09:32:37 +0100

    fix merge error

--- server/data/src/test/kotlin/hu/bsstudio/bssweb/DataTest.kt
+++ server/data/src/test/kotlin/hu/bsstudio/bssweb/DataTest.kt
@@ -1,10 +1,11 @@
 package hu.bsstudio.bssweb
 
+import hu.bsstudio.bssweb.config.BssDataConfig
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ContextConfiguration
 
 @DataJpaTest(properties = ["spring.datasource.url=jdbc:tc:postgresql:16.0-alpine3.18:///db"])
-@ContextConfiguration(classes = [DataModuleConfiguration::class])
+@ContextConfiguration(classes = [BssDataConfig::class])
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class DataTest
