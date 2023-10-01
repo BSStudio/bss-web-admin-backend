f8f4a7a86ee30ec13bcc4c62451d5a65e814c84f
Bence Csik <22349790+csikb@users.noreply.github.com>
2023-10-01 22:13:55 +0100

    move version number to central place

--- server/model/build.gradle.kts
+++ server/model/build.gradle.kts
@@ -7,5 +7,5 @@ dependencies {
     api(project(":server:common"))
     implementation("org.springframework.boot:spring-boot-starter-json")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    testImplementation("io.kotest:kotest-assertions-json:5.7.2")
+    testImplementation(libs.kotestAssertionsJson)
 }
