4017135afdd30c0443fbe0128a6a444cd80d5ea0
Bence Csik <22349790+csikb@users.noreply.github.com>
2023-12-08 22:34:41 +0000

    Update to Java 21

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,10 +10,10 @@ dependencies {
     implementation(libs.plugin.springBoot)
     implementation(libs.plugin.springDependencyManagement)
     implementation(libs.plugin.ktlint)
-    implementation(kotlin("allopen", "1.9.20"))
-    implementation(kotlin("gradle-plugin", "1.9.20"))
+    implementation(kotlin("allopen", "1.9.21"))
+    implementation(kotlin("gradle-plugin", "1.9.21"))
     // required for kotlin plugin jpa
-    implementation(kotlin("reflect", "1.9.20"))
-    implementation(kotlin("stdlib-jdk8", "1.9.20"))
-    implementation(kotlin("noarg", "1.9.20"))
+    implementation(kotlin("reflect", "1.9.21"))
+    implementation(kotlin("stdlib-jdk8", "1.9.21"))
+    implementation(kotlin("noarg", "1.9.21"))
 }
