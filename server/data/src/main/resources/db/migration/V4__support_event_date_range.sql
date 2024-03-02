b99acfc42d3feaba9840a545d2492255c5a3107c
Bence Csik <22349790+csikb@users.noreply.github.com>
2024-03-02 14:45:37 +0000

    add sql

---
+++ server/data/src/main/resources/db/migration/V4__support_event_date_range.sql
@@ -0,0 +1,7 @@
+-- update event.date column to date_from
+ALTER TABLE event
+  RENAME COLUMN date TO date_from;
+
+-- add date_to column, default to date_from
+ALTER TABLE event
+  ADD COLUMN date_to DATE DEFAULT date_from;
