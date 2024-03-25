1759b93ed6ec038fcbb709fe44bb00b8b48ba1c3
Bence Csik <22349790+csikb@users.noreply.github.com>
2024-03-25 15:51:40 +0000

    update

--- buildSrc/src/main/kotlin/spring-app-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/spring-app-conventions.gradle.kts
@@ -20,6 +20,4 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter")
-    // remove
-    implementation("org.springframework.boot:spring-boot-starter-web")
 }
