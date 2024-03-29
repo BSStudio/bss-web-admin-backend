5530e12ad2db38f8e29491f9ee1f568b280aeb59
Bence Csik <22349790+csikb@users.noreply.github.com>
2024-03-29 07:32:55 +0000

    refactor

--- server/data/src/main/resources/db/migration/V8__add_public_views.sql
+++ server/data/src/main/resources/db/migration/V8__add_public_views.sql
@@ -10,7 +10,7 @@ SELECT id,
        archived,
        created_at,
        updated_at
-FROM private.member;
+FROM member;
 
 CREATE VIEW public.video AS
 SELECT
@@ -23,16 +23,16 @@ SELECT
     video.shooting_date_end,
     video.created_at,
     video.updated_at
-FROM private.video
-         LEFT JOIN private.video_url ON video.id = video_url.video_id
-         LEFT JOIN private.video_label ON video.id = video_label.video_id
-         LEFT JOIN private.label ON video_label.label_id = label.id
+FROM video
+         LEFT JOIN video_url ON video.id = video_url.video_id
+         LEFT JOIN video_label ON video.id = video_label.video_id
+         LEFT JOIN label ON video_label.label_id = label.id
 WHERE visible
 GROUP BY video.id;
 
 CREATE VIEW public.crew AS
 SELECT video_id, member_id, position
-FROM private.crew;
+FROM crew;
 
 COMMENT ON VIEW public.crew IS
     E'@foreignKey (video_id) references video (id)\n@foreignKey (member_id) references member (id)';
@@ -46,12 +46,12 @@ SELECT id,
        date_to,
        created_at,
        updated_at
-FROM private.event
+FROM event
 WHERE visible;
 
 CREATE VIEW public.event_video AS
 SELECT event_id, video_id
-FROM private.event_video;
+FROM event_video;
 
 COMMENT ON VIEW public.event_video IS
     E'@foreignKey (event_id) references event (id)\n@foreignKey (video_id) references video (id)';
