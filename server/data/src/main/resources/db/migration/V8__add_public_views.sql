f7055621e0d8d642b0dfb387b8b9fdc4912dcc01
Bence Csik <22349790+csikb@users.noreply.github.com>
2024-03-29 07:32:14 +0000

    refactor

--- server/data/src/main/resources/db/migration/V8__add_public_views.sql
+++ server/data/src/main/resources/db/migration/V8__add_public_views.sql
@@ -1,33 +1,57 @@
 CREATE VIEW public.member AS
-SELECT *
-FROM member;
+SELECT id,
+       url,
+       name,
+       nickname,
+       description,
+       joined_at,
+       role,
+       status,
+       archived,
+       created_at,
+       updated_at
+FROM private.member;
 
 CREATE VIEW public.video AS
-SELECT video.*,
-       array_remove(array_agg(video_url.url), null) AS urls,
-       array_remove(array_agg(label.name), null) AS labels
-FROM video
-         LEFT JOIN video_url ON video.id = video_url.video_id
-         LEFT JOIN video_label ON video.id = video_label.video_id
-         LEFT JOIN label ON video_label.label_id = label.id
-WHERE visible = true
+SELECT
+    video.id,
+    array_remove(array_agg(video_url.url), null) AS urls,
+    video.title,
+    video.description,
+    array_remove(array_agg(label.name), null) AS labels,
+    video.shooting_date_start,
+    video.shooting_date_end,
+    video.created_at,
+    video.updated_at
+FROM private.video
+         LEFT JOIN private.video_url ON video.id = video_url.video_id
+         LEFT JOIN private.video_label ON video.id = video_label.video_id
+         LEFT JOIN private.label ON video_label.label_id = label.id
+WHERE visible
 GROUP BY video.id;
 
 CREATE VIEW public.crew AS
-SELECT *
-FROM crew;
+SELECT video_id, member_id, position
+FROM private.crew;
 
 COMMENT ON VIEW public.crew IS
     E'@foreignKey (video_id) references video (id)\n@foreignKey (member_id) references member (id)';
 
 CREATE VIEW public.event AS
-SELECT *
-FROM event
-WHERE visible = true;
+SELECT id,
+       url,
+       title,
+       description,
+       date_from,
+       date_to,
+       created_at,
+       updated_at
+FROM private.event
+WHERE visible;
 
 CREATE VIEW public.event_video AS
-SELECT *
-FROM event_video;
+SELECT event_id, video_id
+FROM private.event_video;
 
 COMMENT ON VIEW public.event_video IS
     E'@foreignKey (event_id) references event (id)\n@foreignKey (video_id) references video (id)';
