c9ed41bfb50468d1186e0f60f79f538135098764
Bence Csik <22349790+csikb@users.noreply.github.com>
2024-04-20 06:49:10 +0100

    Feature/use k2 compiler (#314)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,10 +10,10 @@ dependencies {
     implementation(libs.plugin.springBoot)
     implementation(libs.plugin.springDependencyManagement)
     implementation(libs.plugin.spotless)
-    implementation(kotlin("allopen", "1.9.23"))
-    implementation(kotlin("gradle-plugin", "1.9.23"))
+    implementation(kotlin("allopen", "2.0.0-RC1"))
+    implementation(kotlin("gradle-plugin", "2.0.0-RC1"))
     // required for kotlin plugin jpa
-    implementation(kotlin("reflect", "1.9.23"))
-    implementation(kotlin("stdlib-jdk8", "1.9.23"))
-    implementation(kotlin("noarg", "1.9.23"))
+    implementation(kotlin("reflect", "2.0.0-RC1"))
+    implementation(kotlin("stdlib-jdk8", "2.0.0-RC1"))
+    implementation(kotlin("noarg", "2.0.0-RC1"))
 }
