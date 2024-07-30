9c199cf067d4ecc645f67e5bd9a8b0e2e351fe39
Bence Csik <22349790+csikb@users.noreply.github.com>
2024-07-31 00:06:55 +0100

    update metrics setup

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("spring-app-conventions")
 }
 
+version = "1.0.0"
+
 dependencies {
     api(project(":server:web"))
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
