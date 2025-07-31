19c91b20f4b1ef336ad201bc4ef530f10f143be7
Bence Csik <22349790+csikb@users.noreply.github.com>
2025-07-31 18:53:23 +0100

    Remove client and integration testing modules, including related configurations and tests. Introduce a new sample integration test using Vitest and Testcontainers for PostgreSQL.

--- client/src/main/kotlin/hu/bsstudio/bssweb/event/client/EventClient.kt
+++
@@ -1,7 +0,0 @@
-package hu.bsstudio.bssweb.event.client
-
-import hu.bsstudio.bssweb.event.operation.EventOperation
-import org.springframework.cloud.openfeign.FeignClient
-
-@FeignClient(name = "event", url = "\${bss.client.url}")
-interface EventClient : EventOperation
--- client/src/main/kotlin/hu/bsstudio/bssweb/member/client/MemberClient.kt
+++
@@ -1,7 +0,0 @@
-package hu.bsstudio.bssweb.member.client
-
-import hu.bsstudio.bssweb.member.operation.MemberOperation
-import org.springframework.cloud.openfeign.FeignClient
-
-@FeignClient(name = "member", url = "\${bss.client.url}")
-interface MemberClient : MemberOperation
