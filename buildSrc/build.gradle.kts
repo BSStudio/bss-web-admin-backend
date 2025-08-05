6096aeb6a133f8d2dc237799af437522f0c61aee
Bence Csik <22349790+csikb@users.noreply.github.com>
2025-08-05 17:12:51 +0100

    Apply suggestion from @csikb

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,8 +10,8 @@ dependencies {
     implementation(libs.plugin.springBoot)
     implementation(libs.plugin.springDependencyManagement)
     implementation(libs.plugin.spotless)
-    implementation(kotlin("allopen", "2.2.0"))
-    implementation(kotlin("gradle-plugin", "2.2.0"))
+    implementation(kotlin("allopen", "2.1.20"))
+    implementation(kotlin("gradle-plugin", "2.1.20"))
     // required for kotlin plugin jpa
     implementation(kotlin("reflect", "2.2.0"))
     implementation(kotlin("stdlib-jdk8", "2.2.0"))
