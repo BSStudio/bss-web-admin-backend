e85682288c8b57b53036f3e425c9b3c2e4d2e279
Bence Csik <22349790+csikb@users.noreply.github.com>
2025-08-05 17:12:56 +0100

    Apply suggestion from @csikb

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation(kotlin("allopen", "2.1.20"))
     implementation(kotlin("gradle-plugin", "2.1.20"))
     // required for kotlin plugin jpa
-    implementation(kotlin("reflect", "2.2.0"))
-    implementation(kotlin("stdlib-jdk8", "2.2.0"))
-    implementation(kotlin("noarg", "2.2.0"))
+    implementation(kotlin("reflect", "2.1.20"))
+    implementation(kotlin("stdlib-jdk8", "2.1.20"))
+    implementation(kotlin("noarg", "2.1.20"))
 }
