2367120a8d10e04161c9cd6cd7a6178bb6fb2653
Bence Csik <22349790+csikb@users.noreply.github.com>
2025-08-24 08:45:31 +0100

    chore: remove unused spring dependency management plugin and update version constraints for security vulnerabilities

--- buildSrc/src/main/kotlin/dependency-management.gradle.kts
+++ buildSrc/src/main/kotlin/dependency-management.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-    id("io.spring.dependency-management")
-}
-
 repositories {
     mavenCentral()
 }
@@ -9,4 +5,31 @@ repositories {
 dependencies {
     add("implementation", platform(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES))
     add("implementation", platform("org.springframework.cloud:spring-cloud-dependencies:2025.0.0"))
+    
+    constraints {
+        add("implementation", "org.springframework:spring-webmvc") {
+            version {
+                strictly("[6.2.10,")
+            }
+            because("CVE-2025-41242")
+        }
+        add("implementation", "org.apache.commons:commons-lang3") {
+            version {
+                strictly("[3.18.0,")
+            }
+            because("CVE-2025-48924")
+        }
+        add("implementation", "com.nimbusds:nimbus-jose-jwt") {
+            version {
+                strictly("[9.37.4,")
+            }
+            because("CVE-2025-53864")
+        }
+        add("implementation", "org.apache.tomcat.embed:tomcat-embed-core") {
+            version {
+                strictly("[10.1.44,")
+            }
+            because("CVE-2025-48989")
+        }
+    }
 }
