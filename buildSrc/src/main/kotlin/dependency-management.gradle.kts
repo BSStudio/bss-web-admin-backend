e6a0250f49f5162de9359c62339ab056528b7790
Bence Csik <22349790+csikb@users.noreply.github.com>
2025-08-24 08:54:42 +0100

    chore: update version constraints for dependencies to address security vulnerabilities

--- buildSrc/src/main/kotlin/dependency-management.gradle.kts
+++ buildSrc/src/main/kotlin/dependency-management.gradle.kts
@@ -9,25 +9,25 @@ dependencies {
     constraints {
         add("implementation", "org.springframework:spring-webmvc") {
             version {
-                strictly("[6.2.10,")
+                require("[6.2.10,)")
             }
             because("CVE-2025-41242")
         }
         add("implementation", "org.apache.commons:commons-lang3") {
             version {
-                strictly("[3.18.0,")
+                require("[3.18.0,)")
             }
             because("CVE-2025-48924")
         }
         add("implementation", "com.nimbusds:nimbus-jose-jwt") {
             version {
-                strictly("[9.37.4,")
+                require("[9.37.4,)")
             }
             because("CVE-2025-53864")
         }
         add("implementation", "org.apache.tomcat.embed:tomcat-embed-core") {
             version {
-                strictly("[10.1.44,")
+                require("[10.1.44,)")
             }
             because("CVE-2025-48989")
         }
