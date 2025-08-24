c6c42c6138d00c3689f17a5765f5804df85dac5c
Bence Csik <22349790+csikb@users.noreply.github.com>
2025-08-24 18:08:59 +0100

    chore: update Docker workflow to refine push and scan conditions

--- server/web/build.gradle.kts
+++ server/web/build.gradle.kts
@@ -18,29 +18,11 @@ dependencies {
     testImplementation("org.springframework.security:spring-security-test")
 
     constraints {
-        add("implementation", "org.springframework:spring-webmvc") {
-            version {
-                require("[6.2.10,)")
-            }
-            because("CVE-2025-41242")
-        }
-        add("implementation", "org.apache.commons:commons-lang3") {
-            version {
-                require("[3.18.0,)")
-            }
-            because("CVE-2025-48924")
-        }
         add("implementation", "com.nimbusds:nimbus-jose-jwt") {
             version {
                 require("[9.37.4,)")
             }
             because("CVE-2025-53864")
-        }
-        add("implementation", "org.apache.tomcat.embed:tomcat-embed-core") {
-            version {
-                require("[10.1.44,)")
-            }
-            because("CVE-2025-48989")
         }
     }
 }
