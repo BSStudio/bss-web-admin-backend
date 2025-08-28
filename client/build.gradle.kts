57896987edafe23bb1d72c4e42af60ba36cfd91e
Bence Csik <22349790+csikb@users.noreply.github.com>
2025-08-28 23:17:29 +0200

    Apply suggestion from @csikb

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("hu.bsstudio.gradle.java-library-convention")
     id("hu.bsstudio.gradle.dependency-management")
-    id("hu.bsstudio.gradle.spotless-convention")
     id("hu.bsstudio.gradle.kotlin-convention")
+    id("hu.bsstudio.gradle.spotless-convention")
 }
 
 dependencies {
