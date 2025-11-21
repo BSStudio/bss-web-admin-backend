29d3a03516e8ee526092d85b9a98f7ed67f04384
Bence Csik <22349790+csikb@users.noreply.github.com>
2025-11-21 14:01:46 +0000

    chore(build): remove deprecated Detekt snapshot repository

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,14 +5,6 @@ plugins {
 
 repositories {
     gradlePluginPortal()
-    // TODO: remove when detekt stable is in gradle plugin portal
-    maven {
-        // https://central.sonatype.org/publish/publish-portal-snapshots/#consuming-via-gradle
-        url = uri("https://central.sonatype.com/repository/maven-snapshots/")
-        content {
-            includeModule("dev.detekt", "detekt-gradle-plugin")
-        }
-    }
 }
 
 dependencies {
@@ -28,45 +20,22 @@ dependencies {
 }
 
 gradlePlugin {
-    plugins.create("dependency-management") {
-        id = "hu.bsstudio.gradle.dependency-management"
-        implementationClass = "hu.bsstudio.gradle.DependencyManagementPlugin"
-    }
-    plugins.create("detekt-convention") {
-        id = "hu.bsstudio.gradle.detekt-convention"
-        implementationClass = "hu.bsstudio.gradle.DetektConventionPlugin"
-    }
-    plugins.create("integration-test-convention") {
-        id = "hu.bsstudio.gradle.integration-test-convention"
-        implementationClass = "hu.bsstudio.gradle.IntegrationTestConventionPlugin"
-    }
-    plugins.create("jacoco-convention") {
-        id = "hu.bsstudio.gradle.jacoco-convention"
-        implementationClass = "hu.bsstudio.gradle.JacocoConventionPlugin"
-    }
-    plugins.create("java-convention") {
-        id = "hu.bsstudio.gradle.java-convention"
-        implementationClass = "hu.bsstudio.gradle.JavaConventionPlugin"
-    }
-    plugins.create("java-library-convention") {
-        id = "hu.bsstudio.gradle.java-library-convention"
-        implementationClass = "hu.bsstudio.gradle.JavaLibraryConventionPlugin"
-    }
-    plugins.create("kotlin-convention") {
-        id = "hu.bsstudio.gradle.kotlin-convention"
-        implementationClass = "hu.bsstudio.gradle.KotlinConventionPlugin"
-    }
-    plugins.create("spotless-convention") {
-        id = "hu.bsstudio.gradle.spotless-convention"
-        implementationClass = "hu.bsstudio.gradle.SpotlessConventionPlugin"
-    }
-    plugins.create("spring-app-convention") {
-        id = "hu.bsstudio.gradle.spring-app-convention"
-        implementationClass = "hu.bsstudio.gradle.SpringAppConventionPlugin"
-    }
-    plugins.create("testing-convention") {
-        id = "hu.bsstudio.gradle.test-convention"
-        implementationClass = "hu.bsstudio.gradle.TestConventionPlugin"
+    mapOf(
+        "dependency-management" to "hu.bsstudio.gradle.DependencyManagementPlugin",
+        "detekt-convention" to "hu.bsstudio.gradle.DetektConventionPlugin",
+        "integration-test-convention" to "hu.bsstudio.gradle.IntegrationTestConventionPlugin",
+        "jacoco-convention" to "hu.bsstudio.gradle.JacocoConventionPlugin",
+        "java-convention" to "hu.bsstudio.gradle.JavaConventionPlugin",
+        "java-library-convention" to "hu.bsstudio.gradle.JavaLibraryConventionPlugin",
+        "kotlin-convention" to "hu.bsstudio.gradle.KotlinConventionPlugin",
+        "spotless-convention" to "hu.bsstudio.gradle.SpotlessConventionPlugin",
+        "spring-app-convention" to "hu.bsstudio.gradle.SpringAppConventionPlugin",
+        "testing-convention" to "hu.bsstudio.gradle.TestConventionPlugin",
+    ).forEach { (id, implementationClass) ->
+        plugins.create(id) {
+            this.id = "hu.bsstudio.gradle.$id"
+            this.implementationClass = implementationClass
+        }
     }
 }
 
