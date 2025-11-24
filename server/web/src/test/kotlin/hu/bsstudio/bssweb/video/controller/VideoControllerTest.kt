0048d967218917877b1a7c6501fe5e08a024d4d1
Bence Csik <22349790+csikb@users.noreply.github.com>
2025-11-24 09:36:19 +0000

    update classpaths and dependencies

--- server/web/src/test/kotlin/hu/bsstudio/bssweb/video/controller/VideoControllerTest.kt
+++ server/web/src/test/kotlin/hu/bsstudio/bssweb/video/controller/VideoControllerTest.kt
@@ -10,7 +10,7 @@ import hu.bsstudio.bssweb.video.service.VideoService
 import io.mockk.every
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
