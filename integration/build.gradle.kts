5c29b0e10b783aa57094d86801de9b323e572277
Bence Csik <22349790+csikb@users.noreply.github.com>
2025-11-25 07:42:18 +0000

    fix(deps): exclude spring-boot-starter-flyway and enforce Jackson 3.x compatibility for spring-cloud-starter-openfeign

--- integration/build.gradle.kts
+++ integration/build.gradle.kts
@@ -12,12 +12,7 @@ dependencies {
         exclude("org.springframework.boot", "spring-boot-starter-flyway")
     }
     integrationTestImplementation(project(":client"))
-    integrationTestImplementation("org.springframework.cloud:spring-cloud-starter-openfeign") {
-        version {
-            require("[5.0, 6.0[")
-            because("Jackson 3.x compatibility")
-        }
-    }
+    integrationTestImplementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     integrationTestImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
     integrationTestImplementation("org.springframework.boot:spring-boot-starter-json")
     integrationTestImplementation("org.springframework.boot:spring-boot-data-jpa-test")
