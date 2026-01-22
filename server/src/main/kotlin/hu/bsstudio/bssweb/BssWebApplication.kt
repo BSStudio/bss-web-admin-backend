a24650b91ff2aa21a36b0198d8a7efe6cc9184c1
Bence <22349790+csikb@users.noreply.github.com>
2026-01-22 23:45:31 +0000

    fix: spotlessApply

--- server/src/main/kotlin/hu/bsstudio/bssweb/BssWebApplication.kt
+++ server/src/main/kotlin/hu/bsstudio/bssweb/BssWebApplication.kt
@@ -3,7 +3,7 @@ package hu.bsstudio.bssweb
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = [ "hu.bsstudio.bssweb" ])
+@SpringBootApplication(scanBasePackages = ["hu.bsstudio.bssweb"])
 class BssWebApplication
 
 fun main(args: Array<String>) {
